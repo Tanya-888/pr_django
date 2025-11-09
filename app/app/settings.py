d97d40a1964c561c98cd10bd8598160dca4e410f
Tanya-888 <vileyka.box@gmail.com>
2025-11-09 19:58:24 +0300

    dj

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('PythonProject4'),
-        'HOST': os.getenv('127.0.0.1'),
-        'PORT': os.getenv('5432'),
-        'USER': os.getenv('postgres'),
-        'PASSWORD': os.getenv('postgres'),
+        'NAME': 'PythonProject4',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
