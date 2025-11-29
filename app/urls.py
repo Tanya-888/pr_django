6ad6c03fbb50a267bc70087271d7a4cf44052af7
Tanya-888 <vileyka.box@gmail.com>
2025-11-29 17:37:41 +0300

    dj

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # обязательный маршрут для админки
+    path('', include('app.urls')),     # или другие маршруты
 ]
+
+
+
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import path
+from . import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
+
